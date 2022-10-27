237ba11a7f666bf9afea91d1a50837dd522b36d4
tzivya <tz0556769591@gmail.com>
2022-10-27 12:49:29 +0300

    stage style singIn commponent

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 import { useState } from "react";
 import React, { useEffect } from "react";
-import SignUp from './SignUp';
+import SignUp from './SignUp'
 import { urlUsers } from '../data/url';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
+
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { formatDiagnostic } from "typescript";
+
 const url= urlUsers;
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-  integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-  crossorigin="anonymous"
-/>
 
 const SignIn = () => {
 
@@ -24,76 +23,131 @@ const SignIn = () => {
     });
 
     const [user, setUser] = useState({
-        IdUser: 0,
+        IdUser: 1,
+        // Name: "",
         FirstName: "",
         LastName: "",
         PasswordUser: 0,
     });
+
+    const [passwordVisible, setPasswordVisible]= useState(false);
+    const elem = document.getElementById('PasswordUser');
     //צריך את זה?
     // למה לא?
     // למה אתה לא עובד?????
-    // tfyguj
-    useEffect(() => {
-        console.log(user.FirstName);
-    }, [user]);
+    // useEffect(() => {
+    //     console.log(user.FirstName);
+    // }, [user]);
 
      // בדיקה האם המשתמש קיים
-    const signIn = () => {
-        console.log("i signIn");
-
+    const signInFunc = () => {
         // GET request using axios with error handling
-        // axios.get(url+ '/Login',user)
-        //     .then(response => {
-        //         if(response == true){
-        //             currentUser = response.data;
-        //             console.log("i signIn");
-        //         }else{
-        //             // { <SignUp />}
-        //             // console.log(response.data);
-        //            <SignUp/> 
-        //         }
-        //     })
-            // .catch(error => {
-            //     console.error('There was an error!', error);
-            // });
+        console.log('firstName: '+user.FirstName+' lastName: ', user.LastName+' password: ', user.PasswordUser)
+        axios.post(url+'/Login',user)
+            .then(response => {
+                // console.log("response: " + JSON.stringify(response.data ) )
+                if(response.data == true){
+                    console.log('i recognize!');
+                    currentUser = response.data;
+                    
+                }else{
+                    console.log("i else");
+                    // { <SignUp />}
+                   <SignUp/> 
+                }
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
     }
+
     //מעדכנת את stateUser  מה form 
     const handleChange = (event) => {
         const { name, value } = event.target;
             let us=user;
             us[name]=value;
             setUser(us);
-        // setUser({ ...user, [name]: value });
+        // console.log('firstName: '+user.FirstName+' lastName: '+user.LastName+' password: '+user.PasswordUser);
     };
+    const handlePasswordVisible = () =>
+    {
+        let text=user.PasswordUser;
+        console.log('text', text);
+        setPasswordVisible(!passwordVisible);
+        console.log('visible', setPasswordVisible);
+        if(passwordVisible)
+        {
+            elem.value = text;
+            //This is required as wihtout it there is a bug that duplicated the first entry if someone decides to show the password
+            elem.value = elem.value.substring(0, text.length - 1)
+            console.log(`Text When Password = Visible: ${text}`)
+        }else
+        {
+            elem.value = "*".repeat(text.length - 1)
+            console.log(`Text When Password = Hidden: ${text}`)
+        }
+    }
 
     return (
-        <div id="center">
-           {/* <b>SignIn </b><br></br>
-            <b>welcome! enter your name and password: </b><br></br>
-            <input name="FirstName" onChange={(e) => handleChange(e)} type="text" placeholder="first name"></input><br></br>
-            <input name="LastName" onChange={(e) => handleChange(e)} type="text" placeholder="last name"></input><br></br>
-            <input name="PasswordUser" onChange={(e) => handleChange(e)} type="password" placeholder="password"></input><br></br>
-            <button onClick={signIn}>Sign In</button> */}
-            
-           
-            <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                {/* <Form.Label>Name</Form.Label> */}
-                <Form.Control name="FirstName" type="text" onChange={(e) => handleChange(e)} placeholder="first name" />
-                </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                {/* <Form.Label>Name</Form.Label> */}
-                <Form.Control name="LastName" type="text" onChange={(e) => handleChange(e)} placeholder="last name" />
+        <>
+        <Form id="center">
+            <Form.Group className="mb-3" /*controlId="formBasicEmail"*/>
+            <Form.Control name="FirstName" type="text" placeholder="firstName" onChange={(e) => handleChange(e)}/>
+                <Form.Text className="text-muted">
+                    fullName or only firstName
+                </Form.Text>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                {/* <Form.Label>Password</Form.Label> */}
-                <Form.Control name="PasswordUser" type="password" onChange={(e) => handleChange(e)} placeholder="password" />
-                </Form.Group>
-                {/* <Button variant="primary" type="submit" onClick={signIn}>
-                    sign in
-                </Button> */}
-            </Form>
-        </div>
-    )
+            <Form.Group className="mb-3" /*controlId="formBasicEmail"*/>
+                <Form.Control name="LastName" type="text" placeholder="lastName" onChange={(e) => handleChange(e)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control id="PasswordUser" name="PasswordUser" type="text" placeholder="Password" onChange={(e) => handleChange(e)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check onClick={()=>handlePasswordVisible} type="checkbox" label="show password" />
+            </Form.Group>
+            <Form.Group>
+                {/* <Form.call func= "" label="Forget passwore" /> */}
+                <Form.Text className="text-muted">
+                Forget passwore
+                </Form.Text>
+            </Form.Group>
+            <Button onClick={(e) => signInFunc(e)} /*type="submit" label="Submit"/* onClick={()=>signInFunc()}*/>
+                sign-in
+            </Button>
+        </Form>
+</>
+
+        // <Form className="center">
+        //     <Form.Group className="mb-3" controlId="formBasicEmail">
+        //         <Form.Label>Email address</Form.Label>
+        //         <Form.Control type="email" placeholder="Enter email" />
+        //         <Form.Text className="text-muted">
+        //         We'll never share your email with anyone else.
+        //         </Form.Text>
+        //     </Form.Group>
+
+        //     <Form.Group className="mb-3" controlId="formBasicPassword">
+        //         <Form.Label>Password</Form.Label>
+        //         <Form.Control type="password" placeholder="Password" />
+        //     </Form.Group>
+        //     <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        //         <Form.Check type="checkbox" label="Check me out" />
+        //     </Form.Group>
+        //     <Button variant="primary" type="submit">
+        //         Submit
+        //     </Button>
+        // </Form>
+    );
+    
+
+        // <div>
+        //    <b>SignIn </b><br></br>
+        //     <b>welcome! enter your name and password: </b><br></br>
+        //     <input name="FirstName" onChange={(e) => handleChange(e)} type="text" placeholder="firs name"></input><br></br>
+        //     <input name="LastName" onChange={(e) => handleChange(e)} type="text" placeholder="last name"></input><br></br>
+        //     <input name="PasswordUser" onChange={(e) => handleChange(e)} type="password" placeholder="password"></input><br></br>
+        //     <button onClick={signIn}>Sign In</button>
+        // </div>
 }
 export default SignIn;
